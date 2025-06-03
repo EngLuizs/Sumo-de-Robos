28e720d9304eb79a21c6f5a0b8719a8dc420702b
Eng Luiz Luz <eng.luizluz@gmail.com>
2025-06-03 14:51:44 -0300

    Update Código - Robô Sumô.ino

--- Código - Robô Sumô.ino
+++ Código - Robô Sumô.ino
@@ -1,62 +1,58 @@
 /*******************************************************************************************************************************
 Autor............: Equipe Eng Robots
 Disciplina.......: APS
-Data.............: 05/2025
+Data.............: 06/2025
 *******************************************************************************************************************************/
 
+#include "Ultrasonic.h"
+#include "BluetoothSerial.h"
 
-//================================================INCLUINDO AS BIBLIOTECAS============================================
-  #include "Ultrasonic.h"
-  #include "BluetoothSerial.h"
+// FUNÇÕES
+  void leiturasIR();
+  void estadoIR();
+  void estadoUltra(); // AGORA RETORNA UM VALOR
+  void leiturasUltra();
+  void virar_pra_esquerda();
+  void virar_pra_direita();
+  void move_pra_frente();
+  void dar_re();
+  void pararMotores();
+  void virar_180();
+  void buzzer_on();
+  void buzzer_off();
 
-//================================================INCLUINDO AS FUNÇÕES================================================
-  //ADICIONANDO FUNÇÕES PARA OS IR
-    void leiturasIR();
-    void mostrarIR();
-    void estadoIR();
+// SENSORES ULTRASSÔNICOS
+  HC_SR04 SensorUF(26,25);
+  HC_SR04 SensorUT(33,32);
+  HC_SR04 SensorUD(4,16);
+  HC_SR04 SensorUE(17,18);
 
-  //ADICIONANDO FUNÇÕES PARA OS 
-    void leiturasUltra();
-    void mostrarUltra();
-    void estadoUltra();
+// PONTE H
+  #define anti_horarioE 23
+  #define horarioE 13
+  #define anti_horarioD 12
+  #define horarioD 27
+  #define PINO_BUZZER 2
 
-  //ADICIONANDO FUNÇÕES PARA OS MOTORES
-    void virar_pra_esquerda();
-    void virar_pra_direita();
-    void move_pra_frente();
-    void dar_re();
-    void pararMotores();
+// SENSORES IR
+  #define IRFD 19
+  #define IRFC 21
+  #define IRFE 22
+  #define IRTD 35
+  #define IRTE 34
 
-//================================================CONFIGURANDO OS PINOS===============================================
-  //DEFININDO OS PINO DO SENSOR ULTRASSÔNICO 
-    HC_SR04 SensorUF(26,25);             // (Trigger,Echo) Sensor Ultrassônico dá Frente
-    HC_SR04 SensorUT(33,32);             // (Trigger,Echo) Sensor Ultrassônico de Trás
-    HC_SR04 SensorUD(4,16);              // (Trigger,Echo) Sensor Ultrassônico do LADO DIREITO olhando da rampa frontal
-    HC_SR04 SensorUE(17,18);             // (Trigger,Echo) Sensor Ultrassônico do LADO ESQUERDO olhando da rampa frontal
-
-  //DEFININDO OS PINO DA PONTE H
-    #define pin1 23
-    #define pin2 13
-    #define pin3 12
-    #define pin4 27
-
-  //DEFININDO OS PINOS DO IR
-    #define IRFD 19
-    #define IRFC 21
-    #define IRFE 22
-    #define IRTD 35
-    #define IRTE 34
-
-//COR DA LEITURA IR
+// LEITURAS
   #define branco 0
   #define preto 1
 
-//VELOCIDADE DO MOTOR
-  #define velocidade 150
-  #define velocidade2 80
+// VELOCIDADES
+  #define velocidade 255
+  #define velocidade2 210
+  #define velocidade3 75
+  #define velocidade4 50
 
-//DISTANCIA DETECTAÇÃO  
-  #define distancia 70
+// DISTÂNCIA DE DETECÇÃO
+  #define distancia 80
 
 //CONFIGURANDO A COMUNICAÇÃO BLUETOOTH 
   BluetoothSerial serialBLE;
@@ -65,250 +61,186 @@ Data.............: 05/2025
   char dados;
   bool conexao;
 
-//ADICIONANDO VARIAVEIS DO IR
+// VARIÁVEIS
   int estadoIRFD, estadoIRFC, estadoIRFE, estadoIRTD, estadoIRTE;
-
-//ADICIONANDO VARIAVEIS DO ULTRASSÔNICO 
   int estadoUF, estadoUT, estadoUD, estadoUE;
 
-void setup() 
-{
-  //INICIANDO A COMUNICAÇÃO SERIAL E BLUETOOTH 
-    Serial.begin(9600);
-    serialBLE.begin("EngRobots");
+void setup() {
+  Serial.begin(9600);
+  serialBLE.begin("EngRobots");
 
-  //CONFIGURAÇÃO MOTORES
-    pinMode(pin1, OUTPUT);
-    pinMode(pin2, OUTPUT);
-    pinMode(pin3, OUTPUT);
-    pinMode(pin4, OUTPUT);
-    pinMode(velocidade, OUTPUT);
+// Motores
+  pinMode(anti_horarioE, OUTPUT);
+  pinMode(horarioE, OUTPUT);
+  pinMode(anti_horarioD, OUTPUT);
+  pinMode(horarioD, OUTPUT);
+  pinMode(velocidade, OUTPUT);
+  pinMode(velocidade2, OUTPUT);
+  pinMode(velocidade3, OUTPUT);
+  pinMode(velocidade4, OUTPUT);
 
-  //CONFIGURAÇÃO SENSORES IR
-    pinMode(IRFD, INPUT);
-    pinMode(IRFC, INPUT);
-    pinMode(IRFE, INPUT);
-    pinMode(IRTD, INPUT);
-    pinMode(IRTE, INPUT);
+// IR
+  pinMode(IRFD, INPUT);
+  pinMode(IRFC, INPUT);
+  pinMode(IRFE, INPUT);
+  pinMode(IRTD, INPUT);
+  pinMode(IRTE, INPUT);
+
+// Buzzer
+  pinMode(PINO_BUZZER, OUTPUT);
+  digitalWrite(PINO_BUZZER, LOW);
+
+  pararMotores();
+  delay(2000);
   
-  //INICIANDO MOTORES DESLIGADOS
-    analogWrite(pin1, 0);
-    analogWrite(pin2, 0);
-    analogWrite(pin3, 0);
-    analogWrite(pin4, 0);
+  analogWrite(anti_horarioE, velocidade4);
+  analogWrite(horarioE, 0);
+  analogWrite(anti_horarioD, velocidade4);
+  analogWrite(horarioD, 0);
+  delay(500);
 }
 
 void loop() 
 {
-  //INICIANDO MOTORES DESLIGADOS
-    analogWrite(pin1, 0);
-    analogWrite(pin2, 0);
-    analogWrite(pin3, 0);
-    analogWrite(pin4, 0);
 
-  //LENDO OS SENSORES
-    leiturasIR();
-    leiturasUltra();
-  
-  //MOSTRANDO DADOS DOS SENSORES NO MONTOR SERIAL
-    //mostrarIR();
-    //mostrarUltra();
+  leiturasUltra(); //LENDO ULTRA
+  estadoUltra();   //AÇÃO DO ULTRA
+  leiturasIR();    //LENDO IR
+  estadoIR();      //AÇÃO IR
 
-  //COMANDOS BLUETOOTH
+//COMANDOS BLUETOOTH
     conexao = serialBLE.available();
     if (conexao == true)
     {
       dados = serialBLE.read();
-      if (dados == 'L')
+      if (dados == 'D')
       {
-        while (dados != 'D')
+        while (true)
         {
-          leiturasUltra();
-          estadoUltra();
-          leiturasIR();
-          estadoIR();
-          dados = serialBLE.read();
+          pararMotores();
+          if (serialBLE.available())
+          {
+            dados = serialBLE.read();
+            if (dados == 'L')
+            {
+              break; // Sai do while
+            }
+          }
         }
       }
     }
 }
 
-//================================================FUNÇÃO LEITURA IR=====================================================
-  void leiturasIR()
-  {
-    //NOMEANDO E LENDO ESTADOS DOS SENSORES
-      estadoIRFD = digitalRead(IRFD);
-      estadoIRFC = digitalRead(IRFC);
-      estadoIRFE = digitalRead(IRFE);
-      estadoIRTD = digitalRead(IRTD);
-      estadoIRTE = digitalRead(IRTE);
+
+// LEITURA DOS SENSORES IR
+  void leiturasIR() {
+    estadoIRFD = digitalRead(IRFD);
+    estadoIRFC = digitalRead(IRFC);
+    estadoIRFE = digitalRead(IRFE);
+    estadoIRTD = digitalRead(IRTD);
+    estadoIRTE = digitalRead(IRTE);
   }
 
-//================================================FUNÇÃO MOSTRAR IR NO SERIAL===========================================
-  void mostrarIR()
-  {
-    Serial.print("Valor Lido pelo IRFD = ");
-    Serial.println(estadoIRFD);
-    delay(1000);
-
-    Serial.print("Valor Lido pelo IRFC = ");
-    Serial.println(estadoIRFC);
-    delay(1000);
-    
-    Serial.print("Valor Lido pelo IRFE = ");
-    Serial.println(estadoIRFE);
-    delay(1000);
-    
-    Serial.print("Valor Lido pelo IRTD = ");
-    Serial.println(estadoIRTD);
-    delay(1000);
-    
-    Serial.print("Valor Lido pelo IRTE = ");
-    Serial.println(estadoIRTE);
-    delay(1000);
-  }
-
-//================================================FUNÇÃO ESTADO IR======================================================
-  void estadoIR()
-  {
-    //SE OS SENSORES IR DA FRENTE ESTIVEREM NA LINHA BRANCA
-    if ((estadoIRFD == branco) || (estadoIRFC == branco) || (estadoIRFE == branco))
-    {
+// AÇÕES COM BASE NOS SENSORES IR
+  void estadoIR() {
+    if ((estadoIRFD == branco) || (estadoIRFC == branco) || (estadoIRFE == branco)) { 
       dar_re();
-      delay(200);
-      do
-      {
-        virar_pra_direita();
-        leiturasUltra();
-      }while(estadoUF <= distancia);    
+      delay(250);
+      virar_180();
     }
-    else 
-    {
-      return;
-    }
-    
-    //SE OS SENSORES IR DE TRÁS ESTIVEREM NA LINHA BRANCA
-    if ((estadoIRTD == branco) || (estadoIRTE == branco))
-    {
-      virar_pra_direita();
-      delay(200);
-      virar_pra_esquerda();
-      delay(200);
+
+    if ((estadoIRTD == branco) || (estadoIRTE == branco)) {
       move_pra_frente();
     } 
-    else
-    {
-      return;
+  }
+
+// ESTADO ULTRASSÔNICO COM RETORNO DE PRIORIDADE
+  void estadoUltra() {
+
+    if (estadoUF <= distancia) {
+      move_pra_frente();
+      buzzer_on();
+      delay(150);
+      buzzer_off();
+
+    } 
+      else if (estadoUD <= distancia) {
+      virar_pra_direita();
+    } 
+      else if (estadoUE <= distancia) {
+      virar_pra_esquerda();
+    } 
+      else if (estadoUT <= distancia) {
+      virar_180();
+    } 
+      else {
+      move_pra_frente_lento();
     }
   }
-//================================================FUNÇÃO LEITURA ULTRASSÔNICO===========================================
-  void leiturasUltra()
-  {
+
+// LEITURA ULTRASSÔNICO
+  void leiturasUltra() {
     estadoUF = SensorUF.distance();
     estadoUT = SensorUT.distance();
     estadoUD = SensorUD.distance();
     estadoUE = SensorUE.distance();
   }
 
-//================================================FUNÇÃO MOSTRAR ULTRASSÔNICO===========================================
-  void mostrarUltra()
-  {
-    Serial.print("Valor lido pelo Ultrassônico da Frente = ");
-    Serial.print(SensorUF.distance());
-    Serial.println(" cm");
-    delay(1000);
-
-    Serial.print("Valor lido pelo Ultrassônico de Trás = ");
-    Serial.print(SensorUT.distance());
-    Serial.println(" cm");
-    delay(1000);
-
-    Serial.print("Valor lido pelo Ultrassônico de Lado Direito = ");
-    Serial.print(SensorUD.distance());
-    Serial.println(" cm");
-    delay(1000);
-
-    Serial.print("Valor lido pelo Ultrassônico de Lado Esquerdo = ");
-    Serial.print(SensorUE.distance());
-    Serial.println(" cm");
-    delay(1000);
+// CONTROLE DOS MOTORES
+  void move_pra_frente() {
+    analogWrite(anti_horarioE, velocidade);
+    analogWrite(horarioE, 0);
+    analogWrite(anti_horarioD, velocidade);
+    analogWrite(horarioD, 0);
   }
 
-//================================================FUNÇÃO ESTADO ULTRASSÔNICO============================================
-  void estadoUltra() {
-  leiturasUltra(); // atualiza os valores dos sensores
-
-  if (estadoUF <= distancia) {
-    move_pra_frente();
-  }
-  else if (estadoUD <= distancia) {
-    do
-    {
-      leiturasUltra();
-      virar_pra_esquerda();
-    } while (estadoUF > distancia);
-
-  }
-  else if (estadoUE <= distancia) {
-    do
-    {
-      leiturasUltra();
-      virar_pra_direita();
-    } while (estadoUF > distancia);
-
-  }
-    else if (estadoUT <= distancia) {
-    do
-    {
-      leiturasUltra();
-      virar_pra_direita();
-    } while (estadoUF > distancia);
-  }
-  else {
-    pararMotores(); // para se nenhum obstáculo estiver perto
-  }
-}
-//================================================FUNÇÃO MOVER PRA FRENTE===============================================
-  void move_pra_frente()
-  {
-    analogWrite(pin1, velocidade);
-    analogWrite(pin2, 0);
-    analogWrite(pin3, velocidade);
-    analogWrite(pin4, 0);
+  void move_pra_frente_lento() {
+    analogWrite(anti_horarioE, velocidade4);
+    analogWrite(horarioE, 0);
+    analogWrite(anti_horarioD, velocidade4);
+    analogWrite(horarioD, 0);
   }
 
-//=========================================ULTRASSÔNICO=======FUNÇÃO DAR RÉ=========================================================
-  void dar_re()
-  {
-    analogWrite(pin1, 0);
-    analogWrite(pin2, velocidade);
-    analogWrite(pin3, 0);
-    analogWrite(pin4, velocidade);
+  void dar_re() {
+    analogWrite(anti_horarioE, 0);
+    analogWrite(horarioE, velocidade);
+    analogWrite(anti_horarioD, 0);
+    analogWrite(horarioD, velocidade);
   }
 
-//================================================FUNÇÃO VIRAR PRA ESQUERDA=============================================
-  void virar_pra_esquerda()
-  {
-    analogWrite(pin1, velocidade);
-    analogWrite(pin2, 0);
-    analogWrite(pin3, 0);
-    analogWrite(pin4, 0);
+  void virar_pra_esquerda() {
+    analogWrite(anti_horarioE, 0);
+    analogWrite(horarioE, velocidade3);
+    analogWrite(anti_horarioD, velocidade2);
+    analogWrite(horarioD, 0);
   }
 
-//================================================FUNÇÃO VIRAR PRA DIREITA==============================================
-  void virar_pra_direita()
-  {
-    analogWrite(pin1, 0);
-    analogWrite(pin2, 0);
-    analogWrite(pin3, velocidade);
-    analogWrite(pin4, 0);
+  void virar_pra_direita() {
+    analogWrite(anti_horarioE, velocidade2);
+    analogWrite(horarioE, 0);
+    analogWrite(anti_horarioD, 0);
+    analogWrite(horarioD, velocidade3);
   }
 
-  //================================================FUNÇÃO VIRAR PRA DIREITA==============================================
-  void pararMotores()
-  {
-    analogWrite(pin1, 0);
-    analogWrite(pin2, 0);
-    analogWrite(pin3, 0);
-    analogWrite(pin4, 0);
+  void virar_180() {
+    analogWrite(anti_horarioE, 0);
+    analogWrite(horarioE, velocidade2);
+    analogWrite(anti_horarioD, velocidade);
+    analogWrite(horarioD, 0);
+  }
+
+  void pararMotores() {
+    analogWrite(anti_horarioE, 0);
+    analogWrite(horarioE, 0);
+    analogWrite(anti_horarioD, 0);
+    analogWrite(horarioD, 0);
+  }
+
+// BUZZER
+  void buzzer_on() {
+    digitalWrite(PINO_BUZZER, HIGH);
+  }
+
+  void buzzer_off() {
+    digitalWrite(PINO_BUZZER, LOW);
   }
